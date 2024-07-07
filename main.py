2a4e87ce428b4a0340cc163fe97dcc76e306f571
Joseph Smith <smithjjosephj@gmail.com>
2024-07-07 20:58:07 +0100

    Convert to GUI

--- main.py
+++ main.py
@@ -5,20 +5,67 @@
 :author: Joseph Smith
 """
 
-import sys
+import customtkinter as ctk
+from typing import Tuple
+from tkinter import messagebox
+
+WINDOW_SIZE: Tuple[int, int] = (600, 300)
+
+mode = None
+
+ctk.set_appearance_mode('system')
+ctk.set_default_color_theme('blue')
+
+
+class App(ctk.CTk):
+    def __init__(self) -> None:
+        super().__init__()
+        self._window_setup()
+
+
+    def _window_setup(self):
+        self.title('Karaoke CD Logger')
+        self.geometry(f"{WINDOW_SIZE[0]}x{WINDOW_SIZE[1]}")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure((0,1), weight=1)
+
+        btn_font = ctk.CTkFont(family=None, size=36, weight='bold')
+        self.input = ctk.CTkButton(self, text='INPUT', font=btn_font,
+                                   command=self._input_app)
+        self.input.grid(row=0, column=0, sticky='NESW', padx=(10,10),
+                        pady=(10,10))
+        self.output = ctk.CTkButton(self, text='OUTPUT', font=btn_font,
+                                    command=self._output_app)
+        self.output.grid(row=0, column=1, sticky='NESW', padx=(10,10),
+                         pady=(10,10))
+
+
+    def _input_app(self):
+        global mode
+        mode = 'input'
+        app.destroy()
+
+
+    def _output_app(self):
+        global mode
+        mode = 'output'
+        app.destroy()
+
 
 if __name__ == '__main__':
-    mode = sys.argv[1]
+    app = App()
+    app.mainloop()
 
+    # Call requested application outside of button callback due to tkinter
+    #  invalid command issues
     if mode == 'input':
         import input
-        app = input.App()
-    elif mode in ['output', 'search']:
+        req_app = input.App()
+    elif mode == 'output':
         import output
-        app = output.App()
+        req_app = output.App()
     else:
-        print("Invalid argument")
-        print("Usage: python3 main.py [input|output|search]")
+        # Assumes user exited out of program
         exit(0)
 
-    app.run()
+    req_app.run()
